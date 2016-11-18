f7963f9d8c193a1b1c9f7b76208d7a26f71801ce
Randy1005 <randychang.10@gmail.com>
2016-11-18 19:21:22 +0800

    login button updated

--- index.php
+++ index.php
@@ -59,11 +59,10 @@
     	<img id="search" src="http://www.century21.com.tw/mobile/images/menu/menu_search_page.png">
     	<input id="search-box" type="text" name="search-box" />
 
-	<!--login button-->
-	<div class="fb-login-button" data-max-rows="1" data-size="xlarge" data-show-faces="false" data-auto-logout-link="true"></div>	
-	<!---->
+	<!--login button-->	
 
-	
+	<div class="fb-login-button" data-max-rows="1" data-size="xlarge" data-show-faces="false" data-auto-logout-link="true" scope = "public_profile,email" onlogin = "checkLoginState();"></div>	
+
 	<!--show fb login status-->
 	<div id="status">
 	</div>
