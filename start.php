8182fca78196a4b92f3a5b3954f037a8811a70a8
StanleyHuangEE <stanley850904@gmail.com>
2016-11-25 19:57:19 +0800

    -test

---
+++ start.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>re1515</title>
+  <link rel="stylesheet" href="css/starter.css">
+  <script src="https://use.fontawesome.com/488b28b092.js"></script>
+  <link rel="stylesheet" type="text/css" href="Semantic-UI/dist/semantic.min.css">
+  <script src="Semantic-UI/dist/semantic.min.js"></script>
+</head>
+
+<body>
+
+<div class="bgimg-1">
+  <div class="caption">
+    <span class="border">Here is 1515</span>
+  </div>
+</div>
+
+<div style="color: #777;background-color:white;text-align:center;padding:50px 80px;text-align: justify;">
+  <h3 style="text-align:center;">Demo</h3>
+  <p>I want a girlfriend QQ</p>
+</div>
+
+<div class="bgimg-2">
+  <div class="caption">
+    <span class="border" style="background-color:transparent;font-size:25px;color: #f7f7f7;">Test</span>
+  </div>
+</div>
+
+<div style="position:relative;">
+  <div style="color:#ddd;background-color:#282E34;text-align:center;padding:50px 80px;text-align: justify;">
+    <p>test</p>
+  </div>
+</div>
+
+<div class="bgimg-3">
+  <div class="caption">
+    <span class="border" style="background-color:transparent;font-size:25px;color: #f7f7f7;">test</span>
+  </div>
+</div>
+
+<div style="position:relative;">
+  <div style="color:#ddd;background-color:#282E34;text-align:center;padding:50px 80px;text-align: justify;">
+    <p>test</p>
+  </div>
+</div>
+
+<div class="bgimg-1">
+  <div class="caption">
+    <span class="border">COOL!</span>
+  </div>
+</div>
+
+</body>
+</html>
