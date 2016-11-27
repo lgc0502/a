0d5ce9ea5ad22818675546b5c47498209b4674e9
StanleyHuangEE <stanley850904@gmail.com>
2016-11-27 17:08:17 +0800

    -test

--- index_1.php
+++ index_1.php
@@ -28,27 +28,29 @@
   <div class="title" id="A">A</div>
   <div class="title">bout u</div>
   <div class="title" id="S">S</div>
-  <div class="context" id="num11">Chiaxun Lee<p>NCKUCSIE<p>Interest:Music<p>motto:沒有做不到的事，只有敢不敢</div>
+	
+	<div class="ui small circular rotate reveal image">
+  	<img src="http://imgur.com/YSYaJdy.png" class="visible content">
+  	<img src="http://imgur.com/LHoO8NJ.png" class="hidden content">
+	</div> 
+	<div class="context" id="num11">Chiaxun Lee<p>NCKUCSIE<p>Interest:Music<p>motto:沒有做不到的事，只有敢不敢</div>
   <div class="context" id="num22">Chiawei Lin<p>成大資管所<p>Interest:Badminton,LOL<p>motto:___。</div>
   <div class="context" id="num33">HsuChia Huang<p>NCKUEE<p>Interest:Badminton<p>motto:魯不是錯,是一種態度.
   </div>
   <div class="context" id="num44">張哲<p>NCKUCSIE<p>Interest:PLAY?<p>motto:KKK</div>
   <div class="context" id="num55">Xu Yan-Ting<p>NCKUCSIE<p>Interest:絕對是coding<p>motto:再吵 把你翻過去喔</div>
-<<<<<<< HEAD:index_1.html
   <img class="photo" id="num1" src="http://imgur.com/YSYaJdy.png" onMouseOver="this.src='http://www.daliulian.net/imgs/image/67/6797064.jpg'" onMouseOut="this.src='http://imgur.com/YSYaJdy.png'">
   <img class="photo" id="num2" src="http://imgur.com/LHoO8NJ.png" onMouseOver="this.src='http://www.daliulian.net/imgs/image/67/6797064.jpg'" onMouseOut="this.src='http://imgur.com/LHoO8NJ.png'">
   <img class="photo" id="num3" src="http://imgur.com/d5NyYaC.png" onMouseOver="this.src='http://www.daliulian.net/imgs/image/67/6797064.jpg'" onMouseOut="this.src='http://imgur.com/d5NyYaC.png'">
   <img class="photo" id="num4" src="http://imgur.com/RAwoTvO.png" onMouseOver="this.src='http://www.daliulian.net/imgs/image/67/6797064.jpg'" onMouseOut="this.src='http://imgur.com/RAwoTvO.png'">
   <img class="photo" id="num5" src="http://imgur.com/T6p91jr.png" onMouseOver="this.src='http://www.daliulian.net/imgs/image/67/6797064.jpg'" onMouseOut="this.src='http://imgur.com/T6p91jr.png'">
   
-=======
   <img class="photo" id="num1" src="http://imgur.com/YSYaJdy.png">
   <img class="photo" id="num2" src="http://imgur.com/LHoO8NJ.png">
   <img class="photo" id="num3" src="http://imgur.com/d5NyYaC.png">
   <img class="photo" id="num4" src="http://imgur.com/RAwoTvO.png">
   <img class="photo" id="num5" src="http://imgur.com/T6p91jr.png">
   </div>
->>>>>>> 0550ed03bd694d55097ce760a2c96e4efb888327:index_1.php
   
  
 
---
+++ index_team.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset= "utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Team Intro</title>
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/team.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="js/jQery.js"></script>
+  <script src="https://use.fontawesome.com/488b28b092.js"></script>
+  <link rel="stylesheet" type="text/css" href="Semantic-UI/dist/semantic.min.css">
+  <script src="Semantic-UI/dist/semantic.min.js"></script>
+</head>
+
+<body>
+	<div id="mySidenav" class="sidenav">
+    <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+    <a href="index.php">1515</a>
+    <a href="index_2.php">新增物品</a>
+    <a href="index_team.php">Our Team</a>
+    <a>Q and A</a>
+  </div>
+  <span class="opennav" onclick="openNav()"> &#9776;</span>
+   <div class="eins">
+    Hello World<br><br>
+    <p>Welcome to Our Innovation</p>
+    </div>
+    
+    <div class="zwei">
+    Target<br><br>
+    <p>Create a platform for you to exchange things</p>
+    <button class="ui red basic button">Red</button>
+    <button class="ui orange basic button">Orange</button>
+    <button class="ui yellow basic button">Yellow</button>
+    <button class="ui basic button">
+      <i class="icon user"></i>
+        Add Friend
+        </button>
+    </div>
+    
+    <div class="drei">
+    <p>
+    <b>Founder</b>  HsuChia Huang<br><br>
+    <img src="https://github.com/StanleyHuangEE/wp2016/blob/gh-pages/profile.jpg?raw=true" width="500px" height="500px">
+    </p>
+    </div>
+    <div class="vier"></div>
+    
+    </body>
+    </html>
+ 	
