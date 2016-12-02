0c09d6d447250d6b0d973a4bb99604c429526cd0
giminey <s952127@gmail.com>
2016-12-02 22:59:54 +0800

    OK by wei

--- index.php
+++ index.php
@@ -38,8 +38,7 @@
 	  <div class="fb-login-button" data-max-rows="1" data-size="xlarge" data-show-faces="false" data-auto-logout-link="true" scope = "public_profile,email" onlogin = "checkLoginState();"></div>	
 
 	<!--show fb login status-->
-    <div id="status">
-    
+    <div id="stage">
   	</div> 
 	<!---->
 <div id="main">  
@@ -90,85 +89,84 @@
 
 <!--facebook login block-->
 <script>
-  // This is called with the results from from FB.getLoginStatus().
-  function statusChangeCallback(response) {
-    console.log('statusChangeCallback');
-    console.log(response);
-    // The response object is returned with a status field that lets the
-    // app know the current login status of the person.
-    // Full docs on the response object can be found in the documentation
-    // for FB.getLoginStatus().
-    if (response.status === 'connected') {
-      // Logged into your app and Facebook.
-      testAPI();
-    } else if (response.status === 'not_authorized') {
-      // The person is logged into Facebook, but not your app.
-      document.getElementById('status').innerHTML = 'Please log ' +
-        'into RE1515.';
-    } else {
-      // The person is not logged into Facebook, so we're not sure if
-      // they are logged into this app or not.
-      document.getElementById('status').innerHTML = 'Please log ' +
-        'into Facebook.';
+    // This is called with the results from from FB.getLoginStatus().
+    var getInfo = new Array();
+    function statusChangeCallback(response) {
+        console.log('statusChangeCallback');
+        console.log(response);
+        getInfo[0] = response.authResponse.accessToken;
+        if (response.status === 'connected') {
+            // Logged into your app and Facebook.
+            testAPI();
+
+        } else if (response.status === 'not_authorized') {
+            alert('not auth!');
+            // The person is logged into Facebook, but not your app.
+        } else {
+            alert('3');
+            // The person is not logged into Facebook, so we're not sure if
+            // they are logged into this app or not.
+        }
     }
-  }
+    // This function is called when someone finishes with the Login
+    // Button.  See the onlogin handler attached to it in the sample
+    // code below.
+    function checkLoginState() {
+        FB.getLoginStatus(function (response) {
+            statusChangeCallback(response);
+        });
+    }
+    window.fbAsyncInit = function () {
+        FB.init({
+            appId: '687068884782423',
+            status: true, // check login status
+            cookie: true,  // enable cookies to allow the server to access
+                           // the session
+            xfbml: true,  // parse social plugins on this page
+            version: 'v2.8' // use graph api version 2.8
+        });
 
-  // This function is called when someone finishes with the Login
-  // Button.  See the onlogin handler attached to it in the sample
-  // code below.
-  function checkLoginState() {
-    FB.getLoginStatus(function(response) {
-      statusChangeCallback(response);
-    });
-  }
+    };
+    // Load the SDK asynchronously
+    (function (d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s);
+        js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+    // Here we run a very simple test of the Graph API after login is
+    // successful.  See statusChangeCallback() for when this call is made.
+    function testAPI() {
+        console.log('Welcome!  Fetching your information.... ');
+        FB.api('/me?fields=id,name,email', function (response) {
+            console.log('Successful login for: ' + response.name);
+            console.log('Email: ' + response.email);
+            console.log(response);
+            getInfo[1] = response.id;
+            getInfo[2] = response.name;
+            getInfo[3]= response.email;
+            showHint();
 
-  window.fbAsyncInit = function() {
-  FB.init({
-    appId      : '687068884782423',
-    cookie     : true,  // enable cookies to allow the server to access 
-                        // the session
-    xfbml      : true,  // parse social plugins on this page
-    version    : 'v2.8' // use graph api version 2.8
-  });
-
-  // Now that we've initialized the JavaScript SDK, we call 
-  // FB.getLoginStatus().  This function gets the state of the
-  // person visiting this page and can return one of three states to
-  // the callback you provide.  They can be:
-  //
-  // 1. Logged into your app ('connected')
-  // 2. Logged into Facebook, but not your app ('not_authorized')
-  // 3. Not logged into Facebook and can't tell if they are logged into
-  //    your app or not.
-  //
-  // These three cases are handled in the callback function.
-
-  FB.getLoginStatus(function(response) {
-    statusChangeCallback(response);
-  });
-
-  };
-
-  // Load the SDK asynchronously
-  (function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));
-
-  // Here we run a very simple test of the Graph API after login is
-  // successful.  See statusChangeCallback() for when this call is made.
-  function testAPI() {
-    console.log('Welcome!  Fetching your information.... ');
-    FB.api('/me', function(response) {
-      console.log('Successful login for: ' + response.name);
-      document.getElementById('status').innerHTML =
-        'Welcome, ' + response.name + '!';
-    });
-  }
-</script>
+        });
+    }
+    function showHint() {
+        if (typeof getInfo[0]=='undefined') {// no input access token
+            document.getElementById("txtHint").innerHTML = "";
+            return;
+        } else {
+            var xmlhttp = new XMLHttpRequest();
+            xmlhttp.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                    document.getElementById("stage").innerHTML = this.responseText;
+                }
+            };
+            xmlhttp.open("GET", "getInfo.php?UserName=" +getInfo[2]+"&UserID="+getInfo[1]+"&UserMail="+getInfo[3], true);
+            xmlhttp.send();
+        }
+    }
+  </script>
 <!--end of facebook login block-->
 
 
--- start.php
+++ start.php
@@ -19,31 +19,34 @@
 </div>
 
 <div style="color: #777;background-color:white;text-align:center;padding:50px 80px;text-align: justify;">
-  <h3>Demo</h3>
-  <p> 先以facebook帳號登入網站，點選左側功能欄中的exchange now欄位，輸入欲與人交換的商品名稱，選擇正確的商品分類，並附上商品圖片，您的商品將會被刊登在主頁，若有人喜歡您的商品，您也剛好喜歡對方的商品，系統將讓你們交換連絡方式，並祝福你們能成功換得自己喜歡的東西喔～</p>
+  <h3>以物易物</h3>
+  <p> <center>以自己已有的物品或服務與別人交換，不一定是等價交換。</center></p>
 </div>
 
 <div class="bgimg-2">
   <div class="caption">
-    <span class="border" style="background-color:transparent;font-size:25px;color: #f7f7f7;">Test</span>
+    <span class="border" style="background-color:transparent;font-size:25px;color: #f7f7f7;">我們的故事</span>
   </div>
 </div>
 
 <div style="position:relative;">
   <div style="color:#ddd;background-color:#282E34;text-align:center;padding:50px 80px;text-align: justify;">
-    <p>iu</p>
+    <p>看著角落靜靜擺著的那隻大熊玩偶，想到自己早已不是玩玩偶的年紀，將他收到回收箱時，突然想起這隻大熊不論在我開心或是難過時，都伴著我哭也伴著我笑，走過了最純真的童年，如果可以將這樣的依靠傳遞給下一個人，何嘗不是一件有意義的事，但若要以金錢變賣，這樣一隻20年的小熊玩偶，會有人願意花錢買嗎？倘若此時我能知道誰需要這隻小熊，並且用一個對方不要但是我需要的東西與我交換，不僅增加樂趣，還能延續物品的價值。
+      我們的團隊希望透過我們的網頁，能使原本即將成為垃圾的東西，得以成為別人的寶物，在這場交易中，自己還能得到需要的物品，或許是一張郵票換一支懷錶，一台收音機換一個老故事，同時也減少了許多垃圾量，為環境資源盡一份心力。
+      
+      </p>
   </div>
 </div>
 
 <div class="bgimg-3">
   <div class="caption">
-    <span class="border" style="background-color:transparent;font-size:25px;color: #f7f7f7;">test</span>
+    <span class="border" style="background-color:transparent;font-size:25px;color: #f7f7f7;">如何易物</span>
   </div>
 </div>
 
 <div style="position:relative;">
   <div style="color:#ddd;background-color:#282E34;text-align:center;padding:50px 80px;text-align: justify;">
-    <p>test</p>
+    <p>先以facebook帳號登入網站，點選左側功能欄中的exchange now欄位，輸入欲與人交換的商品名稱，選擇正確的商品分類，並附上商品圖片，您的商品將會被刊登在主頁，若有人喜歡您的商品，您也剛好喜歡對方的商品，系統將讓你們交換連絡方式，並祝福你們能成功換得自己喜歡的東西喔～</p>
   </div>
 </div>
 
