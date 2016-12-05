9d3fe549f0faac105de86ff7c0046219966d8cd5
StanleyHuangEE <stanley850904@gmail.com>
2016-12-05 20:53:36 +0800

    -test

--- showItem.php
+++ showItem.php
@@ -17,11 +17,11 @@ echo $_SESSION['UserID'];
     <link rel="stylesheet" href="css/sliedNav.css">
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="css/showItem.css">
     <link href=css/justified-nav.css rel=stylesheet>
-    <script src="js/bootstrap.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="js/jQery.js"></script>
-    <script src="js/script-3.js"></script>
+    <script src="js/showItem.js"></script>
     <script src="https://use.fontawesome.com/488b28b092.js"></script>
     <link rel="stylesheet" type="text/css" href="Semantic-UI/dist/semantic.min.css">
     <script src="Semantic-UI/dist/semantic.min.js"></script>
@@ -35,63 +35,86 @@ echo $_SESSION['UserID'];
         <a href="#">ItemCatalog</a>
     </div>
     <span class="opennav" onclick="openNav()"> &#9776;</span>
-    <div class=container>
-        <div class="masthead">
-            <h3 class="text-muted">Project name</h3>
-            <nav>
-                <ul class="nav nav-justified">
-                    <li class="active"><a href="#">Home</a></li>
-                    <li><a href="#">Projects</a></li>
-                    <li><a href="#">Services</a></li>
-                    <li><a href="#">Downloads</a></li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Contact</a></li>
-                </ul>
-            </nav>
-        </div>
 
-        <div class=jumbotron>
-            <h1>Marketing stuff!</h1>
-            <p class=lead>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet.
-            <p><a class="btn btn-lg btn-success" href=# role=button>Get started today</a>
-        </div>
-        <div class=row>
-            <div class=col-lg-4>
-                <h2>Safari bug warning!</h2>
-                <p class=text-danger>As of v8.0, Safari exhibits a bug in which resizing your browser horizontally causes rendering errors in the justified nav that are cleared upon refreshing.
-                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
-                <p><a class="btn btn-primary" href=# role=button>View details &raquo;</a>
-            </div>
-            <div class=col-lg-4>
-                <h2>Heading</h2>
-                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
-                <p><a class="btn btn-primary" href=# role=button>View details &raquo;</a>
-            </div>
-            <div class=col-lg-4>
-                <h2>Heading</h2>
-                <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa.
-                <p><a class="btn btn-primary" href=# role=button>View details &raquo;</a>
-            </div>
-        </div>
-        <div class=row>
-            <div class=col-lg-4>
-                <h2>Safari bug warning!</h2>
-                <p class=text-danger>As of v8.0, Safari exhibits a bug in which resizing your browser horizontally causes rendering errors in the justified nav that are cleared upon refreshing.
-                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
-                <p><a class="btn btn-primary" href=# role=button>View details &raquo;</a>
-            </div>
-            <div class=col-lg-4>
-                <h2>Heading</h2>
-                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
-                <p><a class="btn btn-primary" href=# role=button>View details &raquo;</a>
-            </div>
-            <div class=col-lg-4>
-                <h2>Heading</h2>
-                <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa.
-                <p><a class="btn btn-primary" href=# role=button>View details &raquo;</a>
-            </div>
-        </div>
-            <footer class=footer>
+		<div id="main">
+    	<div class=container>
+        	<div class="masthead">
+          	  <h3 class="text-muted">Project name</h3>
+           	 <nav>
+            	    <ul class="nav nav-justified">
+              	      <li class="active"><a href="#">Home</a></li>
+                	    <li><a href="#">Projects</a></li>
+                  	  <li><a href="#">Services</a></li>
+                   		<li><a href="#">Downloads</a></li>
+                    	<li><a href="#">About</a></li>
+                    	<li><a href="#">Contact</a></li>
+                	</ul>
+            	</nav>
+        	</div>
+
+        	<div class=jumbotron>
+          	  <h1>Marketing stuff!</h1>
+            	<p class=lead>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet.
+            	<p><a class="btn btn-lg btn-success" href=# role=button>Get started today</a>
+        	</div>
+        	<div class=row>
+            	<div class=col-lg-4>
+                	<h2>Safari bug warning!</h2>
+                	<p class=text-danger>As of v8.0, Safari exhibits a bug in which resizing your browser horizontally causes rendering errors in the justified nav that are cleared upon refreshing.
+                	<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
+                	<p><a class="btn btn-primary" href=# role=button>View details &raquo;</a>
+            	</div>
+            	<div class=col-lg-4>
+                	<h2>Heading</h2>
+                	<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
+                	<p><a class="btn btn-primary" href=# role=button>View details &raquo;</a>
+            	</div>
+            	<div class=col-lg-4>
+                	<h2>Heading</h2>
+                	<p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa.
+                	<p><a class="btn btn-primary" href=# role=button>View details &raquo;</a>
+            	</div>
+        	</div>
+       		<div class=row>
+            	<div class=col-lg-4>
+                	<h2>Safari bug warning!</h2>
+                	<p class=text-danger>As of v8.0, Safari exhibits a bug in which resizing your browser horizontally causes rendering errors in the justified nav that are cleared upon refreshing.
+                	<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
+                	<p><a class="btn btn-primary" href=# role=button>View details &raquo;</a>
+            	</div>
+            	<div class=col-lg-4>
+                	<h2>Heading</h2>
+                	<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
+                	<p><a class="btn btn-primary" href=# role=button>View details &raquo;</a>
+            	</div>
+            	<div class=col-lg-4>
+                	<h2>Heading</h2>
+                	<p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa.
+                	<p><a class="btn btn-primary" href=# role=button>View details &raquo;</a>
+            	</div>
+
+							<button id="itemBtn">Open Modal</button>
+
+							<div id="itemModal" class="modal">
+  						  <div class="modal-content">
+    					    <span class="close">x</span>
+    					    <p>Some text in the Modal..</p>
+  						  </div>
+              </div>
+        	</div>
+            
+		</div>
+    
+
+							<button id="itemBtn">Open Modal</button>
+
+							<div id="itemModal" class="modal">
+  						  <div class="modal-content">
+    					    <span class="close">x</span>
+    					    <p>Some text in the Modal..</p>
+  						  </div>
+              </div>
+              <footer class=footer>
                 <p>&copy; Company 2014</footer>
 </body>
 </html>
