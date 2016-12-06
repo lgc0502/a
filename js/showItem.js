320c5ac5d1b23ecf0cb04ee4e87401dae4e75c61
Randy1005 <randychang.10@gmail.com>
2016-12-07 04:17:42 +0800

    mail function completed

--- js/showItem.js
+++ js/showItem.js
@@ -3,11 +3,45 @@ $(document).ready(function(){
         $(this).next().css('display','block');
         $('body').css('background-color','rgba(0, 0, 0, 0.1)');
     });
+
     $(".close").click(function(){
         $(".modal").css('display','none');
         $('body').css('background-color','rgba(255, 255, 255, 1)');
     });
+
     $(".like").click(function(){
-        alert('');
+	   var ID;
+       ID = $(this).parent().next().html();
+		// Assign handlers immediately after making the request,
+	// and remember the jqXHR object for this request
+		var jqxhr = $.ajax( "mail.php" )
+  		//.done(function() {
+	      //alert( "success" );
+		  //  })
+    	//.fail(function() {
+		  //  alert( "error" );
+		//	  })
+	  	//.always(function() {
+		  //    alert( "complete" );
+			//    });
+	   
+	   // Perform other work here ...
+	    
+		// Set another completion function for the request above
+		//jqxhr.always(function() {
+		//	  alert( "second complete" );
+		//});
+		
+		////
+		$.ajax({
+			  method: "GET",
+			  url: "mail.php",
+			  data: { SalerID: ID}
+		})
+		//.done(function( msg ) {
+		//	      alert( "Data Saved: " + msg );
+		//});
+
     });
+
 });
---
+++ mail.php
@@ -0,0 +1,44 @@
+<?php
+	session_start();
+	$SalerID = $_GET['SalerID'];
+	echo $SalerID;
+	header("Content-Type:text/html; charset=utf-8");
+	include 'mysql_config.php';
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	$conn->set_charset("utf8");
+	if($conn->connect_error)
+	{
+		die("connection failed:" . $con->connect_error);	
+	}
+	$buyerID = $_SESSION['UserID'];
+
+	$searchBuyer = $conn->query("SELECT * FROM member WHERE UserID = '$buyerID'");
+	while($result1 = $searchBuyer->fetch_assoc())
+	{
+		$buyer = $result1['UserName'];
+		$buyerMail = $result1['UserMail'];
+	}
+	$searchSaler = $conn->query("SELECT * FROM member WHERE UserID = '$SalerID'");
+	while($result2 = $searchSaler->fetch_assoc())
+	{
+		$saler = $result2['UserName'];
+		$salerMail = $result2['UserMail'];
+	}
+		
+	echo $buyer;
+	echo $saler;
+	echo $salerMail;
+	echo $buyerMail;
+
+	$subject = "Congrats";
+	$message = "$buyer is interested in your product\nemail: $buyerMail";
+	$header = "以物易物 <1515.com>";
+	mail($salerMail,$subject,$message,$header);
+
+	$subject2 = "ready to trade??";
+	$message2 = "you showed interest in $saler's product\nemail: $salerMail";
+	$header2 = "以物易物 <re1515>";
+	mail($buyerMail,$subject2,$message2,$header2);
+	
+		
+?>
