2f9b3809e4a9bdeea3687dafd94d1a8f56e4c5c3
lgc0502 <jallgs0502@gmail.com>
2017-01-07 16:59:23 +0800

    q

--- mysql_config.php
+++ mysql_config.php
@@ -1,15 +1,10 @@
 
 <?php
-<<<<<<< HEAD
   $servername = "localhost";
-=======
   $servename = "localhost";
->>>>>>> 3bf320f9786ca4449a759fc125530ccd6a8f6359
   $username = "wp2016_groupF";
   $password = "XHr3587TfVHVxRYx";
   $dbname = "wp2016_groupF";
 ?>
-<<<<<<< HEAD
 
-=======
->>>>>>> 3bf320f9786ca4449a759fc125530ccd6a8f6359
+
